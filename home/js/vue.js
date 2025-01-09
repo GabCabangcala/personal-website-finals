aed10f9b158416a859ae5f48591b1b5f27f8a623
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-01-09 14:23:16 +0800

    update js

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,11 @@
 const app = Vue.createApp({
     data() {
      return {
-      message: "Hello  WEBPROG SF231 World!"
+      message: "Hello  WEBPROG SF231 World!",
+      vueClass: "pinkBG"
      }
     }
    })
    
    app.mount('#app')
+
