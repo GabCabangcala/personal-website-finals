00c4622b41ca8080996ee78f25c4055bbe47fa50
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-01-27 12:01:26 +0800

    added a second component

--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -9,18 +9,18 @@
   export default {
     data() {
       return {
-        name: 'Apples',
-        message: 'I like apples'
+        name: 'Mangoes',
+        message: 'I like Mangoes'
       }
     }
   };
   </script>
   
-  <style  #app > div {
+  <style>  #app > div {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
+    background-color: lightyellow;
   }
   </style>
