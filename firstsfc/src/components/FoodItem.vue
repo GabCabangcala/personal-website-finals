96757efad79c08655aa053c15a9424acdfc6444e
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-01-27 12:03:42 +0800

    added second component

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="apple">
       <h2>{{ name }}</h2>
       <p>{{ message }}</p>
     </div>
@@ -16,11 +16,12 @@
   };
   </script>
   
-  <style  #app > div {
+  <style>
+    .apple {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
     background-color: lightgreen;
   }
-  </style>
+</style>
