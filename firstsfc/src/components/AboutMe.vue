980aa7dfca792ade371558badd2d14e43167f12a
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-03-04 14:04:36 +0000

    ???

--- firstsfc/src/components/AboutMe.vue
+++ firstsfc/src/components/AboutMe.vue
@@ -17,19 +17,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from '../lib/supabaseClient'
 
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
 </script>
 
 <style scoped>
