b3f3c967fa3df60c69643f6ec1c8c4f16824016d
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-03-04 13:41:45 +0000

    about me ulit

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Hero from './components/Hero.vue'
 import Nav from './components/Nav.vue'
-import Country from './components/AboutMe.vue'
+import AboutMe from './components/AboutMe.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('hero', Hero)
 app.component('nav', Nav)
-app.component('country', Country)
+app.component('about-me', AboutMe)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
