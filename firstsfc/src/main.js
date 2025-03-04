4b6d39ccdad55c06734a4fc74325d03ef49901cc
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-03-04 13:38:57 +0000

    try huhu

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Hero from './components/Hero.vue'
 import Nav from './components/Nav.vue'
-import AboutMe from './components/AboutMe.vue'
+import Country from './components/AboutMe.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('hero', Hero)
 app.component('nav', Nav)
-app.component('about-me', AboutMe)
+app.component('country', Country)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
