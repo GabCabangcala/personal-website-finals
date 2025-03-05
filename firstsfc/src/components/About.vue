9b0df546f61a0f7a383a828d061755182c075957
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-03-05 03:27:50 +0000

    eto?

--- firstsfc/src/components/About.vue
+++ firstsfc/src/components/About.vue
@@ -1,10 +1,11 @@
+<!-- filepath: /workspaces/personal-website-finals/firstsfc/src/components/About.vue -->
 <template>
   <section id="about" class="about-section">
     <div class="container">
       <h2 class="section-title">About Me</h2>
       <div class="about-grid">
         <div class="about-image">
-          <img src="profileImage" alt="Gab's Profile Photo">
+          <img :src="profileImage" alt="Gab's Profile Photo">
         </div>
         <div class="about-content">
           <p>Hello! I'm Gab, a sophomore at Asia Pacific College with a passion for technology and creative design. I'm constantly exploring new ways to blend aesthetics with functionality.</p>
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-import profileImage from '@/assets/prof.jpg'
+import profileImage from '@/assets/Images/prof.jpg'
 
 export default {
   name: 'About',
