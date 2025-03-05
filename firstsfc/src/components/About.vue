7d3071bb30c7545ccfa0a5f6aeb5035e0f9ddee0
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-03-05 03:17:23 +0000

    retry

--- firstsfc/src/components/About.vue
+++ firstsfc/src/components/About.vue
@@ -4,7 +4,7 @@
       <h2 class="section-title">About Me</h2>
       <div class="about-grid">
         <div class="about-image">
-          <img src="/firstsfc/src/components/Images/AMprof.png" alt="Gab's Profile Photo">
+          <img src="/Images/AMprof.png" alt="Gab's Profile Photo">
         </div>
         <div class="about-content">
           <p>Hello! I'm Gab, a sophomore at Asia Pacific College with a passion for technology and creative design. I'm constantly exploring new ways to blend aesthetics with functionality.</p>
